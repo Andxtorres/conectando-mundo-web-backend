f5bd466b5fb00058b6243cb5c93c2ecffed7ee1f
Andrés Fernando Torres Morán <atorres@applab.mx>
2023-09-13 14:47:21 -0600

    Transactions

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI,Depends
+from schema.userschema import User
+from repository import userrepository
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+
 app=FastAPI()
-class User(BaseModel):
-    name:str
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 @app.get("/")
 async def hello_world():
     return {"message":"Hello World"}
@@ -16,3 +24,20 @@ async def hello_name(name:str):
 @app.post("/hello-post")
 async def hello_name(user:User):
     return {"message":f"Hello {user.name}"}
+
+@app.post("/user/create",response_model=User)
+async def create_user(user:User, db: Session = Depends(get_db)):
+    user=userrepository.create_user(db,user)
+    return user
+
+@app.get("/user/list",response_model=list[User])
+async def list_users(db: Session = Depends(get_db)):
+    users=userrepository.list_users(db)
+    return users
+
+@app.get("/user/find/{id}",response_model=User)
+async def find_by_id(db:Session=Depends(get_db),id:int=0):
+    print(id)
+    user=userrepository.find_by_id(db,id)
+    print(user)
+    return user
